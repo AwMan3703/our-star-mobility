e57f3c3d8272ac9cf8a73115725da2f53d0811d2
Aw Man <brambilladaniele@icloud.com>
2024-06-29 05:17:50 +0200

    completed secondary pages

--- js/addTravelPass.js
+++ js/addTravelPass.js
@@ -1,84 +1,117 @@
-import { Human, IDCard } from "./classes.js";
-import { getCards } from "./localStorage.js";
-import { redirect } from "./utility.js";
+import { TravelPass } from "./classes.js";
+import { getCardPasses, getCards, setCardPasses } from "./localStorage.js";
+import { capitalize, redirect } from "./utility.js";
 // CONSTANTS
 const IDCARDS = getCards();
 const card_selector_container = document.getElementById('IDCard-selector');
 const form_inputs = document.querySelectorAll('#TravelPass-form input, #TravelPass-form button');
+const cardSelector_inputs = document.querySelectorAll('#IDCard-selector input, #IDCard-selector button');
 const disclaimer_checkbox = document.getElementById('disclaimer-agree-checkbox');
 // FUNCTIONS
-//TODO / tweak
+function _new_IDCardSelector(card, index) {
+    const container = document.createElement('div');
+    container.classList.add('card-selector-container');
+    container.classList.add('material-box');
+    const card_selector = document.createElement('input');
+    card_selector.id = `card-selector-radio-${crypto.randomUUID()}`;
+    card_selector.type = 'radio';
+    card_selector.name = 'card-selector';
+    card_selector.value = String(index);
+    container.appendChild(card_selector);
+    const label = document.createElement('label');
+    // @ts-ignore
+    label.for = card_selector.id;
+    label.innerText = `${card.number} (${capitalize(card.holder.firstName)} ${capitalize(card.holder.lastName)})`;
+    container.appendChild(label);
+    return container;
+}
 function readFormData() {
     const getValue = (selector) => {
         // @ts-ignore
         return document.querySelector(selector).value;
     };
-    return {
-        number: String(getValue('#TravelPass-form #number')),
-        holder: {
-            firstName: String(getValue('#TravelPass-form #holder-firstName')),
-            lastName: String(getValue('#TravelPass-form #holder-lastName')),
-            // @ts-ignore
-            birthDate: new Date(getValue('#TravelPass-form #holder-birthDate')),
-            TAXID: String(getValue('#TravelPass-form #holder-TAXID'))
-        },
-        // @ts-ignore
-        photoDataURL: URL.createObjectURL(document.querySelector('#TravelPass-form #photo').files[0])
+    const invalidFields = [];
+    const checkValid = (value, name) => {
+        if (value === undefined || !value)
+            invalidFields.push(name);
     };
-}
-//TODO / tweak
-function makeIDCard(data) {
-    const holder = new Human(data.holder.firstName, data.holder.lastName, data.holder.birthDate, data.holder.TAXID);
-    return new IDCard(data.number, holder, data.photoDataURL);
-}
-//TODO / tweak
-function saveIDCard(card) {
-    const savedCards = getCards();
-    savedCards.push(card);
-    setCards(savedCards);
-}
-function getTargetCard() {
-    // @ts-ignore
-    const cardIndex = parseInt(document.querySelector('input[name="card-selector"]:checked').value);
-    return IDCARDS[cardIndex];
-}
-//TODO / tweak
-function addCard() {
-    const formData = readFormData();
-    const card = makeIDCard(formData);
-    saveIDCard(card);
-    alert('Tessera aggiunta');
-    redirect('index.html');
-}
-//TODO / tweak
-function validate_form() {
-    for (const input of form_inputs) {
-        // @ts-ignore
-        if (input.type === 'file') {
-            // @ts-ignore
-            if (String(input.value) == '') {
-                return false;
-            }
+    const from = getValue('#TravelPass-form #from');
+    checkValid(from, 'Origine della tratta');
+    const to = getValue('#TravelPass-form #to');
+    checkValid(to, 'Capolinea della tratta');
+    const line = getValue('#TravelPass-form #line');
+    checkValid(line, 'Numero della linea');
+    const type = getValue('#TravelPass-form #type');
+    checkValid(type, 'Tipologia del titolo');
+    const period = getValue('#TravelPass-form #period');
+    checkValid(period, 'Periodo di validità');
+    const price = getValue('#TravelPass-form #price');
+    checkValid(price, 'Prezzo');
+    const rate = getValue('#TravelPass-form #rate');
+    checkValid(rate, 'Tariffa');
+    const purchase_date = getValue('#TravelPass-form #purchase-date');
+    checkValid(purchase_date, 'Data di acquisto');
+    const purchase_time = getValue('#TravelPass-form #purchase-time');
+    checkValid(purchase_time, 'Ora di acquisto');
+    const expiry_date = getValue('#TravelPass-form #expiry-date');
+    checkValid(expiry_date, 'Data di scadenza');
+    const expiry_time = getValue('#TravelPass-form #expiry-time');
+    checkValid(expiry_time, 'Ora di scadenza');
+    const service = getValue('#TravelPass-form #service');
+    checkValid(service, 'Servizio');
+    if (invalidFields.length > 0) {
+        let message = 'Inserire i dati mancanti: ';
+        for (const field of invalidFields) {
+            message += `\n- ${field}`;
         }
-        else {
-            // @ts-ignore
-            if (input.files == undefined || input.files.length == 0) {
-                return false;
-            }
-        }
-    }
-    return true;
-}
-function form_change() {
-    const invalidFormWarning = document.getElementById('invalid-form-warning');
-    if (!validate_form()) {
-        // @ts-ignore
-        invalidFormWarning.classList.add('visible');
+        alert(message);
+        return null;
     }
     else {
-        // @ts-ignore
-        invalidFormWarning.classList.remove('visible');
+        return {
+            from: String(from),
+            to: String(to),
+            line: String(line),
+            type: String(type),
+            period: String(period),
+            price: parseFloat(String(price).replace(',', '.')),
+            rate: String(rate),
+            purchase: new Date(`${purchase_date} ${purchase_time}`),
+            expiry: new Date(`${expiry_date} ${expiry_time}`),
+            service: String(service)
+        };
     }
+}
+function makeTravelPass(data) {
+    return new TravelPass(data.from, data.to, data.line, data.type, data.period, data.price, data.rate, data.purchase, data.expiry, data.service);
+}
+function saveTravelPass(card, pass) {
+    const savedPasses = getCardPasses(card);
+    savedPasses.push(pass);
+    setCardPasses(card, savedPasses);
+}
+function getTargetCard() {
+    const card_selector = document.querySelector('input[name="card-selector"]:checked');
+    if (!card_selector) {
+        return null;
+    }
+    // @ts-ignore
+    const cardIndex = parseInt(card_selector.value);
+    return IDCARDS[cardIndex];
+}
+function addTravelPass() {
+    const formData = readFormData();
+    if (!formData)
+        return;
+    const pass = makeTravelPass(formData);
+    const targetCard = getTargetCard();
+    if (!targetCard) {
+        alert('Seleziona la tessera a cui abbinare il titolo');
+        return;
+    }
+    saveTravelPass(targetCard, pass);
+    alert('Titolo aggiunto');
+    redirect('index.html');
 }
 // SCRIPT
 // Disable all inputs until the disclaimer is accepted
@@ -87,29 +120,32 @@ disclaimer_checkbox.addEventListener('change', _ => {
     // @ts-ignore
     if (disclaimer_checkbox.checked) {
         form_inputs.forEach(form_input => { form_input.removeAttribute('disabled'); });
+        cardSelector_inputs.forEach(cardSelector_input => { cardSelector_input.removeAttribute('disabled'); });
         // @ts-ignore
-        document.getElementById('IDCard-form').classList.remove('disabled');
+        document.getElementById('TravelPass-form').classList.remove('disabled');
+        // @ts-ignore
+        document.getElementById('IDCard-selector').classList.remove('disabled');
     }
     else {
         form_inputs.forEach(form_input => { form_input.setAttribute('disabled', "true"); });
+        cardSelector_inputs.forEach(cardSelector_input => { cardSelector_input.setAttribute('disabled', "true"); });
         // @ts-ignore
-        document.getElementById('IDCard-form').classList.add('disabled');
+        document.getElementById('TravelPass-form').classList.add('disabled');
+        // @ts-ignore
+        document.getElementById('IDCard-selector').classList.add('disabled');
     }
 });
 // Add a selector list to target cards
 for (let i = 0; i < IDCARDS.length; i++) {
-    // Input.name="card-selector"
-    const card_selector = document.createElement('input');
-    card_selector.type = 'radio';
-    card_selector.name = 'card-selector';
-    card_selector.value = String(i);
     // @ts-ignore
-    card_selector_container.appendChild(card_selector);
+    card_selector_container.appendChild(_new_IDCardSelector(IDCARDS[i], i));
 }
-form_inputs.forEach(form_input => { form_input.addEventListener('change', form_change); });
 form_inputs.forEach(form_input => { form_input.setAttribute('disabled', "true"); });
+cardSelector_inputs.forEach(cardSelector_input => { cardSelector_input.setAttribute('disabled', "true"); });
 // @ts-ignore
-document.getElementById('IDCard-form').classList.add('disabled');
+document.getElementById('TravelPass-form').classList.add('disabled');
+// @ts-ignore
+document.getElementById('IDCard-selector').classList.add('disabled');
 const submit_button = document.querySelector('#TravelPass-form #submit-button');
 // @ts-ignore
-submit_button.onclick = addCard;
+submit_button.onclick = addTravelPass;
