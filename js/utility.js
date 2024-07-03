d3696ca67f518a05984b544afe5e6cd10bcc83fe
Aw Man <88689516+AwMan3703@users.noreply.github.com>
2024-07-03 02:38:24 +0200

    lmao

--- js/utility.js
+++ js/utility.js
@@ -1,5 +1,7 @@
 // UTILITY FUNCTIONS
-export const redirect = (location) => { window.location.href = location; };
+export const redirect = (location) => {
+    window.location.href = location;
+};
 export function capitalize(text) {
     return text[0].toUpperCase() + text.substring(1, text.length);
 }
--- js/utility.ts
+++ js/utility.ts
@@ -1,6 +1,7 @@
 // UTILITY FUNCTIONS
 
-export const redirect = (location: string | URL) => { window.location.href = location }
+export const redirect = (location: string | URL) => { // @ts-ignore
+    window.location.href = location }
 
 export function capitalize(text: string) {
     return text[0].toUpperCase() + text.substring(1, text.length)}
